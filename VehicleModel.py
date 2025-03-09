20776b68ceef8f7001a3f36cad08686943bece5c
IshDeshpa <ishdeshpa@gmail.com>
2025-03-08 18:12:20 -0600

    vehicle model

--- VehicleModel.py
+++ VehicleModel.py
@@ -1,6 +1,9 @@
 from pint import UnitRegistry
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+
+import time
 
 UR = UnitRegistry()
 
@@ -16,36 +19,129 @@ class VehicleModel:
 
         self.frictional_loss = ((self.weight * (self.mu + (self.mu2*(self.avg_velocity)))) * self.avg_velocity).to('watts')
 
-        self.loss = (self.drag_loss + self.frictional_loss)
+        self.power_loss = (self.drag_loss + self.frictional_loss)
 
-        return self.loss
+        return self.power_loss
 
     def pwr_gen(self):
         self.back_gen = (self.back_subarray_ct * self.v_mpp * self.i_mpp * math.cos(math.radians(self.back_incident_angle))).to('watts')
 
         self.lr_gen = (self.lr_subarray_ct * self.v_mpp * self.i_mpp * math.cos(math.radians(self.lr_incident_angle))).to('watts') * 2
 
-        self.gen = (self.back_gen + self.lr_gen)
+        self.power_gen = (self.back_gen + self.lr_gen)
 
-        return self.gen
+        return self.power_gen
     
     def print_vars(self):
         for key, value in vars(self).items():
             print(f"{key}: {value}")
     
     def run(self):
-        self.print_vars()
-
         power_loss = self.pwr_loss()
 
         power_gen = self.pwr_gen()
 
         return power_gen, power_loss
+    
+    def get_grad(self, key_x, loss_function, step=0.0001):
+        curr_key_x = getattr(self, key_x)
+        self.run()
+        curr_out = loss_function(self)
+
+        new_key_x = curr_key_x + (step * curr_key_x.units)
+        setattr(self, key_x, new_key_x)
+        self.run()
+        new_out = loss_function(self)
+
+        grad = (new_out - curr_out) / step
+
+        # Reset key
+        setattr(self, key_x, curr_key_x)
+
+        return grad
+    
+    def get_optimal_gd(self, 
+                    key_x, 
+                    loss_function, 
+                    lr, 
+                    tolerance=0.0001):
+        initial_val = getattr(self, key_x)
+
+        points = []
+        
+        print("=====================================")
+
+        # gradient descent
+        while True:
+            grad = self.get_grad(key_x, loss_function)
+            print(f"Gradient: {grad}")
+
+            update = lr * grad
+            print(f"Update: {update}")
+            
+            updated_x = getattr(self, key_x) - (update * initial_val.units)
+            updated_x = updated_x.to(initial_val.units)
+            setattr(self, key_x, updated_x)
+            print(f"New {key_x}: {getattr(self, key_x)}")
+
+            loss = loss_function(self)
+            print(f"Loss: {loss}")
+            if abs(loss) < tolerance:
+                break
+
+            points.append((getattr(self, key_x).m, loss))
+
+            print("=====================================")
+
+        final_val = getattr(self, key_x)
+
+        # maintain the initial value
+        setattr(self, key_x, initial_val)
+
+        # Plotting
+        x = [point[0] for point in points]
+        y = [point[1] for point in points]
+
+        plt.plot(x, y)
+        plt.xlabel(key_x)
+        plt.ylabel("Loss")
+        plt.title(f"{key_x} vs Loss")
+        plt.show()
+
+        return final_val
+    
+    def get_optimal_sweep(self, key_x, step_size, min, max, loss_function):
+        initial_val = getattr(self, key_x)
+
+        points = []
+
+        for i in np.arange(min, max, step_size):
+            setattr(self, key_x, i * initial_val.units)
+            self.run()
+            loss = loss_function(self)
+            points.append((i, loss))
+
+        setattr(self, key_x, initial_val)
+
+        # Plotting
+        x = [point[0] for point in points]
+        y = [point[1] for point in points]
+
+        plt.plot(x, y)
+        plt.xlabel(key_x)
+        plt.ylabel("Loss")
+        plt.title(f"{key_x} vs Loss")
+        plt.show()
+
+        return x[y.index(min(y))]
+
+def loss_function(model):
+    return (model.power_gen - model.power_loss).m ** 2
 
 # Inputs
 inputs_dict = {
     # General
-    "avg_velocity": 9 * UR.mps, # m/s | average velocity
+    "avg_velocity": 16 * UR.mps, # m/s | average velocity
 
     # Drag
     "drag_coeff": 0.1305, # drag coefficient
@@ -77,15 +173,6 @@ inputs_dict = {
     "battery_E_max": 5240 * UR.kilowatt_hour, # | fully charged energy of battery
 }
 
-# # Functions
-# def get_grad(setter):
-
-
-
-# # vary speed until power gen == power loss
-# def get_optimal():
-#     # gradient descent
-    
-
 model = VehicleModel(inputs_dict)
-model.run()
+# model.get_optimal_sweep("avg_velocity", 0.01, 0, 22, loss_function)
+model.get_optimal_gd("lr_incident_angle", loss_function, 1)
