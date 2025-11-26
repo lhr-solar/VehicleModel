93f485532171ab1680e5c425fc07ab695ea8906a
IshDeshpa <ishdeshpa@gmail.com>
2025-11-25 20:30:15 -0600

    wheel-by-wheel rolling resistance

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ def parse_yaml(yaml_path: str) -> dict[str, Quantity[float]]:
 
 def main():
     m = VehicleModel(parse_yaml("params.yaml"))
-    m.add_model(SCPRollingResistanceModel())
+    # Two front wheels, one rear wheel
+    m.add_model(SCPRollingResistanceModel(m.params["f_wheel_weight"]))
+    m.add_model(SCPRollingResistanceModel(m.params["f_wheel_weight"]))
+    m.add_model(SCPRollingResistanceModel(m.params["r_wheel_weight"]))
     m.add_model(SCPDragModel())
     m.add_model(SCPArrayModel())
   
--- models/rr.py
+++ models/rr.py
@@ -3,15 +3,17 @@ from models.base import EnergyModel
 from typing import override
 from pint import Quantity 
 
+# A rolling resistance model for an individual wheel
 class SCPRollingResistanceModel(EnergyModel):
-    def __init__(self): 
+    def __init__(self, wheel_weight): 
         super().__init__()
+        self.wheel_weight = wheel_weight
 
     @override
     def update(self, params: dict[str, Quantity[float]], timestep: Quantity[float]) -> Quantity[float]:
         # TODO
         params['total_mu'] = params['mu_rr'] + params['mu2_rr'] * params['velocity']
-        params['normal_force'] = (params['weight'] * params['grav_accel']).to('N')
+        params['normal_force'] = (self.wheel_weight * params['grav_accel']).to('N')
         params['rolling_resistance_force'] = (params['normal_force'] * params['total_mu'])
         params['rolling_resistance_power'] = -(params['rolling_resistance_force'] * params['velocity']).to('watts')
         return (params['rolling_resistance_power'] * timestep).to('Wh')
