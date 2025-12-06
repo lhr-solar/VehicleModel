45d94b65ed38a15911554e6dde5b3303fe8db4ca
Ishan Deshpande <ishdeshpa@gmail.com>
2025-12-06 16:26:34 -0600

    wheel-by-wheel rolling resistance (#12)
    * wheel-by-wheel rolling resistance
    
    * Refactored to only use one RR model with separate parameters for the front and rear calculations
    
    * updated params

--- models/rr.py
+++ models/rr.py
@@ -3,6 +3,7 @@ from models.energy_model import EnergyModel
 from typing import override
 from pint import Quantity 
 
+# A rolling resistance model for an individual wheel
 class SCPRollingResistanceModel(EnergyModel):
     def __init__(self): 
         super().__init__()
@@ -11,7 +12,15 @@ class SCPRollingResistanceModel(EnergyModel):
     def update(self, params: dict[str, Quantity[float]], timestep: Quantity[float]) -> Quantity[float]:
         # TODO
         params['total_mu'] = params['mu_rr'] + params['mu2_rr'] * params['velocity']
-        params['normal_force'] = (params['weight'] * params['grav_accel']).to('N')
-        params['rolling_resistance_force'] = (params['normal_force'] * params['total_mu'])
-        params['rolling_resistance_power'] = -(params['rolling_resistance_force'] * params['velocity']).to('watts')
-        return (params['rolling_resistance_power'] * timestep).to('Wh')
+
+        params['f_normal_force'] = (params['f_wheel_weight'] * params['grav_accel']).to('N')
+        params['f_rr_force'] = (params['f_normal_force'] * params['total_mu'])
+        params['f_rr_power'] = -(params['f_rr_force'] * params['velocity']).to('watts')
+
+        params['r_normal_force'] = (params['r_wheel_weight'] * params['grav_accel']).to('N')
+        params['r_rr_force'] = (params['r_normal_force'] * params['total_mu'])
+        params['r_rr_power'] = -(params['r_rr_force'] * params['velocity']).to('watts')
+
+        params['rr_power'] = 2 * params['f_rr_power'] + params['r_rr_power']
+
+        return (params['rr_power'] * timestep).to('Wh')
