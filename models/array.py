ea5420bd1673d48baa832ab559ba942c741b96fd
Jana Daouk <janadaouk@hotmail.com>
2025-12-06 14:12:22 -0600

    Responding to comment

--- models/array.py
+++ models/array.py
@@ -58,14 +58,17 @@ class SCPArrayModelWithIncidence(EnergyModel):
         # sunlight factor 0–1
         factor = self._incidence_factor(params)
 
-        # base max power
-        base_power = params["num_cells"] * params["p_mpp"] * params["cell_efficiency"]
+       # base max power
+    base_power = params["num_cells"] * params["p_mpp"] * params["cell_efficiency"]
 
-        # actual array power this timestep
-        params["array_power"] = base_power * factor
+    params["array_power"] = base_power * factor
 
-        # accumulate energy (Power × time)
-        params["total_array_energy"] += params["array_power"] * timestep
+        # energy produced this timestep (Power × time)
+    params["array_energy"] = params["array_power"] * timestep
 
-        # return energy produced this timestep
-        return params["array_power"] * timestep
+    # accumulate total array energy over the whole race
+    params["total_array_energy"] += params["array_energy"]
+
+    # return energy produced this timestep
+    return params["array_energy"]
+
