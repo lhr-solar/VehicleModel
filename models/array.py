13c20dac4ecb40d58e2fbb9119b0cd471caf1bd4
jana-daouk <janadaouk@hotmail.com>
2026-01-10 15:54:50 -0600

    fixed lat/long. made only 1 model customizable and now calcs with angle of sun.

--- models/array.py
+++ models/array.py
@@ -3,17 +3,9 @@ from typing import override
 from pint import Quantity 
 import math
 
+
+
 class SCPArrayModel(EnergyModel):
-    def __init__(self):
-        super().__init__()
-
-    @override
-    def update(self, params: dict[str, Quantity[float]], timestep: Quantity[float]) -> Quantity[float]:
-        params["array_power"] = params["num_cells"] * params["p_mpp"] * params["cell_efficiency"]
-        return params["array_power"] * timestep
-
-
-class SCPArrayModelWithIncidence(EnergyModel):
     def __init__(self):
         super().__init__()
 
