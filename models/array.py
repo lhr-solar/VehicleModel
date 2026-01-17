ed446d6865781404056737c20dbdfdb384db2e76
IshDeshpa <ishdeshpa@gmail.com>
2026-01-17 12:01:59 -0600

    fixed

--- models/array.py
+++ models/array.py
@@ -45,5 +45,23 @@ class SCPArrayModel(EnergyModel):
 
     @override
     def update(self, params: dict[str, Quantity[float]], timestep: Quantity[float]) -> Quantity[float]:
-        params["array_power"] = params["num_cells"] * params["p_mpp"] * params["cell_efficiency"]
-        return params["array_power"] * timestep
+        # Updates array power based on sun angle
+        # accumulates energy into total_array_energy.
+
+        # sunlight factor 0–1
+        factor = self._incidence_factor(params)
+
+       # base max power
+        base_power = params["num_cells"] * params["p_mpp"] * params["cell_efficiency"]
+
+        params["array_power"] = base_power * factor
+
+            # energy produced this timestep (Power × time)
+        params["array_energy"] = params["array_power"] * timestep
+
+        # accumulate total array energy over the whole race
+        params["total_array_energy"] += params["array_energy"]
+
+        # return energy produced this timestep
+        return params["array_energy"]
+
